8279f371a0dc06e1982c7bc080db0ea3bbcb58f8
VimukthiPramudantha <vimukthipramudanth@gmail.com>
2024-06-25 21:13:38 +0530

    javascript

--- cal.js
+++ cal.js
@@ -1,13 +1,63 @@
-// function equal(){
-//     document.getElementById("eq").
-// }
-// function numberOne(){
-//     document.getElementById("one").innerText;
-// }
-// function numberTwo(){
-//     document.getElementById("two").innerText;
-// }
-// function numberTree(){
-//     document.getElementById("tree").innerText;
-// }
+document.addEventListener("DOMContentLoaded", function() {
 
+    const buttonValues = {
+        'mul':'*',
+        'divide':'/',
+        'clear':'C',
+        'seven':'7',
+        'eight':'8',
+        'nine':'9',
+        'mines':'-',
+        'fore':'4',
+        'five':'5',
+        'six':'6',
+        'add':'+',
+        'one':'1',
+        'two':'2',
+        'tree':'3',
+        'eq':'=',
+        'zero':'0',
+        'dot':'.'
+    }
+
+        for(const[id,value]of Object.entries(buttonValues)){
+            const button =document.getElementById(id);
+            if (button){
+                button.textContent= value;
+                button.setAttribute('data-value',value);
+
+                button.onclick = function(num) {
+
+                    // if(parseInt(document.getElementById("display").innerText) !=0){ 
+                    //     // document.getElementById("display").innerText+=value;
+                    // }
+                    // else
+                    console.log(value)
+                     if(id === 'clear') {
+                        document.getElementById("display").innerText="";
+                         
+                    }
+                    else if(id === 'mul'){
+                        document.getElementById("display").innerText='*';
+                    }
+                    else if(id === 'divide'){
+                        document.getElementById("display").innerText='/';
+                    }
+                    else if(id === 'mines'){
+                        document.getElementById("display").innerText='-';
+                    }
+                    else if(id === 'add'){
+                        document.getElementById("display").innerText='+';
+                    }
+                    // else if(id === 'eq'){
+                    //     document.getElementById("display").innerText='=';
+                    // }
+                    else {
+                        document.getElementById("display").innerText+=value;
+                    }
+                }   
+            }  
+        }
+        
+       
+    });
