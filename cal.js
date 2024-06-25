43e2349e4791c99830cf389b139758f278b9e9ac
VimukthiPramudantha <vimukthipramudanth@gmail.com>
2024-06-25 18:39:06 +0530

    Second commit

---
+++ cal.js
@@ -0,0 +1,13 @@
+// function equal(){
+//     document.getElementById("eq").
+// }
+// function numberOne(){
+//     document.getElementById("one").innerText;
+// }
+// function numberTwo(){
+//     document.getElementById("two").innerText;
+// }
+// function numberTree(){
+//     document.getElementById("tree").innerText;
+// }
+
